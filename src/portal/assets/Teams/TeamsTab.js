5b13a8ef35098f90a375586c344710fc08bbc411
Liam Snow <liamsnow03@gmail.com>
2018-06-12 11:38:07 -0700

    Teams Tab 1.1

--- src/portal/assets/Teams/TeamsTab.js
+++ src/portal/assets/Teams/TeamsTab.js
@@ -5,10 +5,11 @@ var TeamsTab = new RegisteredTab("Teams", null, teamsInitEvery, teamsExit, true)
 //  ----------------------------------------  Init  -------------------------------------------------\\
 var teamsSnapshot;
 document.addEventListener('DOMContentLoaded', function () {
-	firebase.app().firestore().collection("Teams").onSnapshot(function (snapshot) {
-		teamsSnapshot = snapshot;
-		teamsDraw(false);
-	});
+	firebase.app().firestore().collection("Teams")
+		.onSnapshot(function (snapshot) {
+			teamsSnapshot = snapshot;
+			teamsDraw(false);
+		});
 });
 
 function teamsInitEvery() {
@@ -47,8 +48,12 @@ function teamsDraw(teamsDrawTransition) {
 						<div class="mdc-typography--body2" style="width: 90%;">` + data.desc + `</div>
 						<i onclick="teamsJL('` + id + `', ` + teams.memberInTeam(users.getCurrentUid(), id) + `)" data-mdc-auto-init="MDCIconToggle" class="mdc-icon-toggle material-icons" style="color: rgb(80, 80, 80); position: absolute; right: 20px; top: 0px;" role="button" aria-pressed="false" aria-label="` + (teams.memberInTeam(users.getCurrentUid(), id) ? 'Leave Team' : 'Join Team') + `" aria-label-delay="0.2s"><i class="material-icons" style="font-size: 150%; position: absolute; left: 7px; top: 7px;">` + (teams.memberInTeam(users.getCurrentUid(), id) ? 'call_made' : 'call_received') + `</i></i>
 					</div>
-					<div class="mdc-card__action-icons">
+					<div class="mdc-card__action-icons" style="position: relative;">
+						<i data-mdc-auto-init="MDCIconToggle" onclick="teamsExpandCollapseMembers('` + id + `')" class="material-icons mdc-icon-toggle" role="button" style="color: rgb(80, 80, 80); position: absolute; left: 0; font-size: 200%;" aria-label="Show Members">expand_more</i>
 						<i data-mdc-auto-init="MDCIconToggle" onclick="toggleMenu(null, true)" class="mdc-icon-toggle material-icons" style="color: rgb(80, 80, 80);" role="button" aria-pressed="false">more_vert</i>
+					</div>
+					<div class="teamsMemberList" style="overflow: hidden; opacity: 0; max-height: 0px; transition: opacity .8s, max-height 1s; transition-timing-function: cubic-bezier(.2, 0, .2, 1);">
+						` + teamsGetMembersHtml(id) + `
 					</div>
 					<ul class="dropdown-menu-c dropdown-menu be-connections" style="padding: 0;" data-menu-offset="0 -27">
 						<li class="mdc-elevation--z10">
@@ -158,5 +163,45 @@ function teamsJL(team, leave) {
 	else data.members.push(users.getCurrentUid());
 
 	firebase.app().firestore().collection("Teams").doc(team).set(data);
+}
+//  ----------------------------------------    -------------------------------------------------\\
+
+
+
+
+
+//  ----------------------------------------  Expand/Collapse Members  -------------------------------------------------\\
+function teamsExpandCollapseMembers(id) {
+	try {
+		var btn = event.srcElement;
+		var doExpand = (btn.innerHTML == "expand_more");
+		btn.innerHTML = doExpand ? "expand_less" : "expand_more";
+		var lst = btn.parentNode.parentNode.querySelector('.teamsMemberList');
+		if (doExpand) {
+			lst.style.opacity = "1";
+			lst.style.maxHeight = (40 * lst.childNodes.length) + "px";
+		}
+		else {
+			lst.style.opacity = "0";
+			lst.style.maxHeight = "0px";
+		}
+	} catch (err) { console.log(err); }
+}
+function teamsGetMembersHtml(id) {
+	var html = '';
+	var membs = teams.getMembers(id);
+	for (var i = 0; i < membs.length; i++) {
+		var mem = users.getUser(membs[i]);
+		html += `
+			<div style="width: 100%; background: ` + ((i % 2 == 0) ? '#efefef' : '#ffffff') + `; height: 40px; position: relative;">
+				<img style="border-radius: 50%; width: 35px; margin: 2.5px 5px 0 2.5px;" src="` + (mem.avatar || "../assets/img/iconT.png") + `">
+				<div style="position: absolute; left: 42px; top: 8.5px;">
+					<span style="font-size: 120%; font-weight: 600;">` + (mem.username || "Username") + `</span>
+					<span style="font-size: 90%; margin-left: 2px;">` + (mem.name || (mem.username || "Name")) + `</span>
+				</div>
+			</div>
+		`;
+	}
+	return html;
 }
 //  ----------------------------------------    -------------------------------------------------\\
