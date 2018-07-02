539a3ecd9001a609f9abe1910c211035c983cd5d
Liam Snow <liamsnow03@gmail.com>
2018-07-02 11:32:07 -0700

    ...

--- src/portal/assets/Manuals/Manuals.js
+++ src/portal/assets/Manuals/Manuals.js
@@ -1,4 +1,34 @@
 // Manuals.js
 
-new RegisteredTab("Manuals", null, null, null, false);
+new RegisteredTab("Manuals", Manuals.firstInit, Manuals.everyInit, Manuals.exitInit, true);
 
+var ManualsTab = new class ManualsTab {
+	constructor() {
+		var t = this;
+		t.fab = new FabHandler(document.querySelector('#Manual-Add-Fab'));
+		t.snapshot;
+	}
+
+	firstInit() {
+		var t = this;
+		firebase.app().firestore().collection("Manuals").onSnapshot(function (snap) {
+			t.snapshot = snap;
+
+
+		});
+	} 
+
+	everyInit() {
+		var t = this;
+		t.fab.tabSwitch();
+
+		console.log(fab);
+
+		showMainLoader(false);
+	}
+
+	exitInit() {
+		var t = this;
+		fab.tabExit();
+	}
+}
