b344f9046ae155f5e414cfc39ee78fc1a60cbd34
Liam Snow <liamsnow03@gmail.com>
2018-08-06 11:54:16 -0700

    Compression and Start of Engineering Page

--- src/assets/vit/js/vitality.js
+++ src/assets/vit/js/vitality.js
@@ -1,145 +1,125 @@
-/*!
- * Vitality v2.1.0 (http://themes.startbootstrap.com/vitality-v2.1.0)
- * Copyright 2013-2018 Start Bootstrap
- * Purchase a license to use this theme at (https://wrapbootstrap.com)
- */
-/*!
- * Vitality v2.0.3 (http://themes.startbootstrap.com/vitality-v2.0.3)
- * Copyright 2013-2017 Start Bootstrap
- * Purchase a license to use this theme at (https://wrapbootstrap.com)
- */
-
 // Load WOW.js on non-touch devices
-var isPhoneDevice = "ontouchstart" in document.documentElement;
-$(document).ready(function() {
-  if (isPhoneDevice) {
-    //mobile
-  } else {
-    //desktop
-    // Initialize WOW.js
-    wow = new WOW({
-      offset: 50
-    })
-    wow.init();
-  }
-});
+try {
+	var isPhoneDevice = "ontouchstart" in document.documentElement;
+	$(document).ready(function () {
+		if (isPhoneDevice) {
+			//mobile
+		} else {
+			//desktop
+			// Initialize WOW.js
+			wow = new WOW({
+				offset: 50
+			})
+			wow.init();
+		}
+	});
+} catch (err) { console.error(err); }
 
-(function($) {
-  "use strict"; // Start of use strict
+(function ($) {
+	"use strict"; // Start of use strict
 
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 69)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
-  });
+	// Smooth scrolling using jQuery easing
+	try {
+		$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+			if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+				if (target.length) {
+					$('html, body').animate({
+						scrollTop: (target.offset().top - 69)
+					}, 1000, "easeInOutExpo");
+					return false;
+				}
+			}
+		});
+	} catch (err) { console.error(err); }
 
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
+	// Closes responsive menu when a scroll trigger link is clicked
+	try {
+		$('.js-scroll-trigger').click(function () {
+			$('.navbar-collapse').collapse('hide');
+		});
+	} catch (err) { console.error(err); }
 
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 70
-  });
+	// Activate scrollspy to add active class to navbar items on scroll
+	try {
+		$('body').scrollspy({
+			target: '#mainNav',
+			offset: 70
+		});
+	} catch (err) { console.error(err); }
 
-  // Collapse Navbar
-  var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-shrink");
-    } else {
-      $("#mainNav").removeClass("navbar-shrink");
-    }
-  };
-  // Collapse now if page is not at top
-  navbarCollapse();
-  // Collapse the navbar when page is scrolled
-  $(window).scroll(navbarCollapse);
 
   // Activates floating label headings for the contact form
+	try {
   $("body").on("input propertychange", ".floating-label-form-group", function(e) {
     $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
   }).on("focus", ".floating-label-form-group", function() {
     $(this).addClass("floating-label-form-group-with-focus");
   }).on("blur", ".floating-label-form-group", function() {
     $(this).removeClass("floating-label-form-group-with-focus");
-  });
+			});
+	} catch (err) { console.error(err); }
+
 
   // Owl Carousel Settings
-  $(".team-carousel").owlCarousel({
-    items: 3,
-    navigation: true,
-    pagination: false,
-    navigationText: [
-      "<i class='fa fa-angle-left'></i>",
-      "<i class='fa fa-angle-right'></i>"
-    ],
-  });
+	try {
+		$(".team-carousel").owlCarousel({
+			items: 3,
+			navigation: true,
+			pagination: false,
+			navigationText: [
+				"<i class='fa fa-angle-left'></i>",
+				"<i class='fa fa-angle-right'></i>"
+			],
+		});
+	} catch (err) { console.error(err); }
 
-  $(".portfolio-carousel").owlCarousel({
-    singleItem: true,
-    navigation: true,
-    pagination: false,
-    navigationText: [
-      "<i class='fa fa-angle-left'></i>",
-      "<i class='fa fa-angle-right'></i>"
-    ],
-    autoHeight: true,
-    mouseDrag: false,
-    touchDrag: false,
-    transitionStyle: "fadeUp"
-  });
+	
+	try {
+		$(".portfolio-carousel").owlCarousel({
+			singleItem: true,
+			navigation: true,
+			pagination: false,
+			navigationText: [
+				"<i class='fa fa-angle-left'></i>",
+				"<i class='fa fa-angle-right'></i>"
+			],
+			autoHeight: true,
+			mouseDrag: false,
+			touchDrag: false,
+			transitionStyle: "fadeUp"
+		});
+	} catch (err) { console.error(err); }
 
-  $(".testimonials-carousel, .mockup-carousel").owlCarousel({
-    singleItem: true,
-    navigation: true,
-    pagination: true,
-    autoHeight: true,
-    navigationText: [
-      "<i class='fa fa-angle-left'></i>",
-      "<i class='fa fa-angle-right'></i>"
-    ],
-    transitionStyle: "backSlide"
-  });
+	try {
+		$(".testimonials-carousel, .mockup-carousel").owlCarousel({
+			singleItem: true,
+			navigation: true,
+			pagination: true,
+			autoHeight: true,
+			navigationText: [
+			  "<i class='fa fa-angle-left'></i>",
+			  "<i class='fa fa-angle-right'></i>"
+			],
+			transitionStyle: "backSlide"
+		});
 
-  $(".portfolio-gallery").owlCarousel({
-    items: 3,
-  });
+		$(".portfolio-gallery").owlCarousel({
+			items: 3,
+		});
+	} catch (err) { console.error(err); }
 
-  // Magnific Popup jQuery Lightbox Gallery Settings
-  $('.gallery-link').magnificPopup({
-    type: 'image',
-    gallery: {
-      enabled: true
-    },
-    image: {
-      titleSrc: 'title'
-    }
-  });
-
-  // Magnific Popup Settings
-  $('.mix').magnificPopup({
-    type: 'image',
-    image: {
-      titleSrc: 'title'
-    }
-  });
-
-  // Vide - Video Background Settings
-  $('header.video').vide({
-    mp4: "mp4/camera.mp4",
-    poster: "img/agency/backgrounds/bg-mobile-fallback.jpg"
-  }, {
-    posterType: 'jpg'
-  });
 
 })(jQuery); // End of use strict
+
+// Collapse Navbar
+function getScroll() {
+	return (document.documentElement.scrollTop || document.body.scrollTop);
+}
+setInterval(function () {
+	if (getScroll() > 0)
+		$("#mainNav").addClass("navbar-shrink");
+	else
+		$("#mainNav").removeClass("navbar-shrink");
+}, 100);
