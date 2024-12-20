9802426c1a1dd85dc57ce99a626434fd1c0b82c7
BreakerBots <132958980+BreakerBots-public@users.noreply.github.com>
2024-12-20 10:15:07 -0800

    hide store

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ var pages = {
   // "/hours/people": ["hours/people.html", getPeopleInjection],
   "/thankyou": ["thankyou.html"],
   "/dashboard": ["dashboard2.html"],
-  "/store": ["store.html"],
+  //"/store": ["store.html"],
   // "/breakerlib": ["breakerlib.html"],
   // "/weeklyupdates": ["weeklyupdates.html"],
   // "/joinus": ["joinus.html"]
